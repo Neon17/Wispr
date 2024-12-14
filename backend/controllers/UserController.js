1a6cbdd1152d1a8dcf4d5524c9c041d40ffd14a1
Neon17 <neon.neupane.9@gmail.com>
2024-12-14 19:59:37 +0545

    FEAT: showChatHead returned in getAllMessages as in discussion of Github

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,94 +4,94 @@ const Message = require('../models/Message');
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
 
-const getJoinedGroups = asyncErrorHandler(async(req,res,next)=>{
+const getJoinedGroups = asyncErrorHandler(async (req, res, next) => {
     let allGroups = await Group.find({})
         .populate({
             path: 'members'
         }).exec();
-    
-    let groups=[];
+
+    let groups = [];
     let groupIds = [];
-    allGroups.forEach((group)=>{
-        for (i=0;i<group.members.length;i++){
-            if (group.members[i]._id.toString()==req.user._id.toString()) {
+    allGroups.forEach((group) => {
+        for (i = 0; i < group.members.length; i++) {
+            if (group.members[i]._id.toString() == req.user._id.toString()) {
                 groupIds.push(group._id);
                 groups.push(group);
                 break;
             }
         }
     })
-    for (let j=0;j<groupIds.length;j++){
-        await Group.findByIdAndUpdate(groupIds[j],{onlineStatus: true});
+    for (let j = 0; j < groupIds.length; j++) {
+        await Group.findByIdAndUpdate(groupIds[j], { onlineStatus: true });
     }
     return groups;
 })
 
-const getLatestMessages = (async(groups)=>{
+const getLatestMessages = (async (groups) => {
     let messages = [];
     let message;
-    for (let i=0;i<groups.length;i++){
-        message = await Message.find({groupId: groups[i]._id}).exec();
-        messages.push(message[message.length-1]);
+    for (let i = 0; i < groups.length; i++) {
+        message = await Message.find({ groupId: groups[i]._id }).exec();
+        messages.push(message[message.length - 1]);
     }
     return messages;
 })
 
-const nameJoinedGroups = (groups,id)=>{
+const nameJoinedGroups = (groups, id) => {
     let status = 0, groupCount = 0;
     let groupName = "";
     let nameCount = 0;
     let temp;
-    for (i=0;i<groups.length;i++){
-        if (groups[i].members.length==2) status = 1;
+    for (i = 0; i < groups.length; i++) {
+        if (groups[i].members.length == 2) status = 1;
         nameCount = 0;
         groupName = "";
-        if (status==1){
-            temp =  (groups[i].members[0]._id.toString()==id.toString())?1:0;
-            if (groups[i].members[temp].middleName!=null)
-                groupName = groups[i].members[temp].firstName + " "+ groups[i].members[temp].middleName + " "+ groups[i].members[temp].lastName;
-            else groupName = groups[i].members[temp].firstName + " "+ groups[i].members[temp].lastName;
-            groupCount= groupCount+1;
+        if (status == 1) {
+            temp = (groups[i].members[0]._id.toString() == id.toString()) ? 1 : 0;
+            if (groups[i].members[temp].middleName != null)
+                groupName = groups[i].members[temp].firstName + " " + groups[i].members[temp].middleName + " " + groups[i].members[temp].lastName;
+            else groupName = groups[i].members[temp].firstName + " " + groups[i].members[temp].lastName;
+            groupCount = groupCount + 1;
         }
-        else if ((status == 0)&&(groups[i].name==null)){
-            for (j=0;j<3;j++){
-                if (groups[i].members[j]._id!=id) {
-                    if (nameCount!=0) groupName = groupName + ", ";
+        else if ((status == 0) && (groups[i].name == null)) {
+            for (j = 0; j < 3; j++) {
+                if (groups[i].members[j]._id != id) {
+                    if (nameCount != 0) groupName = groupName + ", ";
                     groupName = groupName + groups[i].members[j].firstName;
-                    nameCount = nameCount+1;
+                    nameCount = nameCount + 1;
                 }
-                if (nameCount==2){
-                    groupName = groupName + `+${groups[i].members.length-2}`;
+                if (nameCount == 2) {
+                    groupName = groupName + `+${groups[i].members.length - 2}`;
                     break;
                 }
             }
         }
-        if (groups[i].name==null) groups[i].name = groupName;
+        if (groups[i].name == null) groups[i].name = groupName;
         status = 0;
     }
     return groups;
 }
 
-exports.getAllUsers = asyncErrorHandler(async(req,res,next)=>{
-    let users = await User.find({_id: {$ne:{_id: req.user._id}}});
+exports.getAllUsers = asyncErrorHandler(async (req, res, next) => {
+    let users = await User.find({ _id: { $ne: { _id: req.user._id } } });
     res.status(200).json({
         status: 'success',
         data: users
     })
 })
 
-exports.uploadProfilePicture = asyncErrorHandler(async(req,res,next)=>{
+exports.uploadProfilePicture = asyncErrorHandler(async (req, res, next) => {
     if (!req.body) throw new Error('Request body should be in valid JSON format');
     if (req.files) {
         let filePath = `/upload/images/${req.file.fileName}`;
-        res.render("image",{
+        res.render("image", {
             filePath
         })
     }
     //save fileName in db
     let updatedUser = await User.findByIdAndUpdate(req.user._id, {
         profilePicture: req.fileName
-    }, {new: true, runValidators: false});
+    }, { new: true, runValidators: false });
 
     res.status(200).json({
         status: 'success',
@@ -99,17 +99,17 @@ exports.uploadProfilePicture = asyncErrorHandler(async(req,res,next)=>{
     })
 })
 
-exports.profile = asyncErrorHandler(async(req,res,next)=>{
+exports.profile = asyncErrorHandler(async (req, res, next) => {
     res.status(200).json({
         status: 'success',
         data: req.user
     })
 })
 
-exports.showAllGroupList = asyncErrorHandler(async(req,res,next)=>{
+exports.showAllGroupList = asyncErrorHandler(async (req, res, next) => {
     //It shows group list and latest message of each group
-    let groups = await getJoinedGroups(req,res,next);
-    groups = nameJoinedGroups(groups,req.user._id);
+    let groups = await getJoinedGroups(req, res, next);
+    groups = nameJoinedGroups(groups, req.user._id);
     let latestMessages = await getLatestMessages(groups);
     res.status(200).json({
         status: 'success',
@@ -120,50 +120,50 @@ exports.showAllGroupList = asyncErrorHandler(async(req,res,next)=>{
     })
 })
 
-exports.fetchAllUnknownUsers = asyncErrorHandler(async(req,res,next)=>{
+exports.fetchAllUnknownUsers = asyncErrorHandler(async (req, res, next) => {
     // It fetches all users but not group that user has started chatting with
-    let groups = await getJoinedGroups(req,res,next);
-    let allUsers = await User.find({_id: {$not: {$eq: req.user._id}}});
+    let groups = await getJoinedGroups(req, res, next);
+    let allUsers = await User.find({ _id: { $not: { $eq: req.user._id } } });
     let users = [];
-    allUsers.forEach((user)=>{
+    allUsers.forEach((user) => {
         let st = 0;
-        for (i=0;i<groups.length;i++){
-            if (groups[i].members.length!=2) continue;
-            if ((groups[i].members[0]._id.toString()==user._id.toString())||((groups[i].members[1]._id.toString()==user._id.toString()))){
+        for (i = 0; i < groups.length; i++) {
+            if (groups[i].members.length != 2) continue;
+            if ((groups[i].members[0]._id.toString() == user._id.toString()) || ((groups[i].members[1]._id.toString() == user._id.toString()))) {
                 st = 1; break;
             }
         }
-        if (st==0) users.push(user);
+        if (st == 0) users.push(user);
     })
     res.status(200).json({
-        status : 'success',
+        status: 'success',
         data: users
     })
 })
 
-exports.showSearchUsername = asyncErrorHandler(async(req,res,next)=>{
+exports.showSearchUsername = asyncErrorHandler(async (req, res, next) => {
     // here we have to search for direct message or group message
     // in direct message, username should be considered, if group message, name should be like username1,username2+6 others
     // let username = req.body.username;
-    let groups = await getJoinedGroups(req,res,next);
-    groups = nameJoinedGroups(groups,req.user._id);
+    let groups = await getJoinedGroups(req, res, next);
+    groups = nameJoinedGroups(groups, req.user._id);
     res.status(200).json({
         status: 'success',
         data: groups
     })
 })
 
-exports.createGroup = asyncErrorHandler(async(req,res,next)=>{
+exports.createGroup = asyncErrorHandler(async (req, res, next) => {
     if (!req.body.id) throw new Error("At least one user is needed to form a group");
 
-    if (typeof(req.body.id)=='object') {
+    if (typeof (req.body.id) == 'object') {
         let ids = [];
         ids.push(req.user._id);
-        req.body.id.map((singleId)=>{
-            if (singleId!=req.user._id)
+        req.body.id.map((singleId) => {
+            if (singleId != req.user._id)
                 ids.push(singleId);
         })
-        let prepareObj={};
+        let prepareObj = {};
         if (req.body.name)
             prepareObj.name = req.body.name;
         prepareObj.members = ids;
@@ -171,20 +171,20 @@ exports.createGroup = asyncErrorHandler(async(req,res,next)=>{
         return res.status(200).json({
             status: 'success',
             data: group
-        }) 
+        })
     }
-    else if (typeof(req.body.id)=='string'){
-        if (req.body.id==req.user._id) throw new Error("You can't create group with yourself");
+    else if (typeof (req.body.id) == 'string') {
+        if (req.body.id == req.user._id) throw new Error("You can't create group with yourself");
         let user = await User.findById(req.user._id);
-        if (user.length==0) throw new Error("Selected User doesn't exists");
+        if (user.length == 0) throw new Error("Selected User doesn't exists");
         let previousGroups = await Group.find({});
         let status = 0;
 
         //It is necessary to check previousGroup formed because every direct message is treated as group here
-        previousGroups.map((previousGroup)=>{
+        previousGroups.map((previousGroup) => {
             status = 0;
-            if (previousGroup.members.length==2){
-                if ((previousGroup.members[0].toString==req.body.id)||(previousGroup.members[1].toString==req.body.id)) 
+            if (previousGroup.members.length == 2) {
+                if ((previousGroup.members[0].toString == req.body.id) || (previousGroup.members[1].toString == req.body.id))
                     throw new Error('That group already exists');
             }
         })
@@ -202,44 +202,44 @@ exports.createGroup = asyncErrorHandler(async(req,res,next)=>{
     })
 })
 
-exports.addMemberInGroup = asyncErrorHandler(async(req,res,next)=>{
+exports.addMemberInGroup = asyncErrorHandler(async (req, res, next) => {
     if (!req.body.id) throw new Error("At least one group should be selected");
     if (!req.body.members) throw new Error("At least one member is needed to form a group");
     let group = await Group.findById(req.body.id);
-    if (group.length==0) throw new Error("Group with that ID doesn't exists");
+    if (group.length == 0) throw new Error("Group with that ID doesn't exists");
     let memberArray = [];
-    for (i=0;i<group.members.length;i++){
+    for (i = 0; i < group.members.length; i++) {
         memberArray.push(group.members[i]);
     }
     let addStatus = 0;
-    if (typeof(req.body.members)=='object'){
-        for (i=0;i<req.body.members.length;i++){
-            if (req.body.members[i].toString()==req.user._id.toString()) addStatus = 1;
+    if (typeof (req.body.members) == 'object') {
+        for (i = 0; i < req.body.members.length; i++) {
+            if (req.body.members[i].toString() == req.user._id.toString()) addStatus = 1;
             let u = await User.findById(req.body.members[i]);
-            if (u.length==0) addStatus = 1;
-            for (j=0;j<memberArray.length;j++){
-                if (memberArray[j]==req.body.members[i]){
+            if (u.length == 0) addStatus = 1;
+            for (j = 0; j < memberArray.length; j++) {
+                if (memberArray[j] == req.body.members[i]) {
                     addStatus = 1;
                     break;
                 }
             }
-            if (addStatus==0) memberArray.push(req.body.members[i]);
+            if (addStatus == 0) memberArray.push(req.body.members[i]);
         }
     }
-    else if (typeof(req.body.members)=='string'){
+    else if (typeof (req.body.members) == 'string') {
         let u = await User.findById(req.body.members);
-        if (u.length==0) throw new Error("Member with that ID doesn't exists");
+        if (u.length == 0) throw new Error("Member with that ID doesn't exists");
         let addStatus = 0;
-        for (j=0;j<memberArray.length;j++){
-            if (memberArray[j]==req.body.members){
+        for (j = 0; j < memberArray.length; j++) {
+            if (memberArray[j] == req.body.members) {
                 addStatus = 1;
                 break;
             }
         }
-        if (addStatus==1) throw new Error("Member with that ID is already in the group");
+        if (addStatus == 1) throw new Error("Member with that ID is already in the group");
         else memberArray.push(req.body.members);
     }
-    let updatedGroup = await Group.findByIdAndUpdate(req.body.id,{members: memberArray},{new:true, runValidators: true})
+    let updatedGroup = await Group.findByIdAndUpdate(req.body.id, { members: memberArray }, { new: true, runValidators: true })
         .populate('members').exec();
     res.status(200).json({
         status: 'success',
@@ -247,10 +247,10 @@ exports.addMemberInGroup = asyncErrorHandler(async(req,res,next)=>{
     })
 })
 
-exports.getAllMessages = asyncErrorHandler(async(req,res,next)=>{
+exports.getAllMessages = asyncErrorHandler(async (req, res, next) => {
     //req.body contains groupId
     if (!req.body.groupId) throw new Error('Message can only be seen in the group');
-    let groupMessages = await Message.find({groupId: req.body.groupId})
+    let groupMessages = await Message.find({ groupId: req.body.groupId })
         .populate('senderId').exec();
     let group = await Group.findById(req.body.groupId).exec();
     let readStatus = [];
@@ -260,48 +260,48 @@ exports.getAllMessages = asyncErrorHandler(async(req,res,next)=>{
     let seenBy = [];
     let showChatHead = [];
 
-    for (let i=0;i<group.members.length;i++){
-        if (group.members[i].toString()==req.user._id.toString()){
+    for (let i = 0; i < group.members.length; i++) {
+        if (group.members[i].toString() == req.user._id.toString()) {
             defaultReadStatus.push(true);
         }
         else defaultReadStatus.push(false);
     }
 
 
-    for (i=0;i<messages.length;i++){
+    for (i = 0; i < messages.length; i++) {
 
-        if (messages[i].senderId._id.toString()==req.user._id.toString())
+        if (messages[i].senderId._id.toString() == req.user._id.toString())
             messages[i].isUser = true;
         else messages[i].isUser = false;
 
-        if (messages[i].readStatus.length==0){
-            await Message.findByIdAndUpdate(messages[i]._id, {readStatus: defaultReadStatus});
+        if (messages[i].readStatus.length == 0) {
+            await Message.findByIdAndUpdate(messages[i]._id, { readStatus: defaultReadStatus });
         }
         else {
             readStatus = [];
-            for (j=0;j<messages[i].readStatus.length;j++){
-                if (group.members[j].toString()==req.user._id.toString()){
+            for (j = 0; j < messages[i].readStatus.length; j++) {
+                if (group.members[j].toString() == req.user._id.toString()) {
                     readStatus.push(true);
                 }
                 else readStatus.push(messages[i].readStatus[j]);
             }
-            await Message.findByIdAndUpdate(messages[i]._id, {readStatus: readStatus});
+            await Message.findByIdAndUpdate(messages[i]._id, { readStatus: readStatus });
         }
     }
-    if (messages.length==0) messages = null;
+    if (messages.length == 0) messages = null;
 
     //For Retrieving Seen
     let temp = 0, tempSeenBy = [];
-    for (j=0;j<messages.length;j++){
+    for (j = 0; j < messages.length; j++) {
         tempSeenBy = []; temp = 0;
-        for (k=0;k<messages[j].readStatus.length;k++){
-            if (messages[j].readStatus[k] && !(group.members[k].toString()==req.user._id.toString())){
+        for (k = 0; k < messages[j].readStatus.length; k++) {
+            if (messages[j].readStatus[k] && !(group.members[k].toString() == req.user._id.toString())) {
                 temp = 1; break;
             }
         }
-        if (temp==0) tempSeenBy.push(group.onlineStatus);
+        if (temp == 0) tempSeenBy.push(group.onlineStatus);
         else {
-            for (k=0;k<messages[j].readStatus.length;k++){
+            for (k = 0; k < messages[j].readStatus.length; k++) {
                 tempSeenBy.push(messages[j].readStatus[k]);
             }
         }
@@ -310,24 +310,44 @@ exports.getAllMessages = asyncErrorHandler(async(req,res,next)=>{
 
     //For Making Ease in Frontend
     temp = 0; tempSeenBy = [];
-    //----Needs to solve soon
-
+    for (i = 0; i < (seenBy.length - 1); i++) {
+        tempSeenBy = []
+        if ((seenBy[i].length > 1) && (seenBy[i+1].length > 1)) {
+            for (j = 0; j < seenBy[i].length; j++) {
+                if (j>=seenBy[i+1].length){
+                    tempSeenBy.push(seenBy[i][j]);
+                    continue;
+                }
+                if ((seenBy[i][j]) && (seenBy[i + 1][j]))
+                    tempSeenBy.push(false);
+                else tempSeenBy.push(seenBy[i][j]);
+            }
+        }
+        else if (seenBy[i].length > 1){
+            for (j = 0; j < seenBy[i].length; j++)
+                tempSeenBy.push(seenBy[i][j]);
+        }
+        else tempSeenBy.push(seenBy[i]);
+        console.log(tempSeenBy);
+        showChatHead.push(tempSeenBy);
+    }
+    showChatHead.push(seenBy[seenBy.length-1]);
 
     res.status(200).json({
         status: 'success',
         data: messages,
-        seenBy
+        showChatHead
     })
 })
 
-exports.sendMessage = asyncErrorHandler(async (req,res,next)=>{
+exports.sendMessage = asyncErrorHandler(async (req, res, next) => {
     //req.body contains groupId, message
     if (!req.body.groupId || !req.body.message) throw new Error('Non-empty message can only be sent with the group');
     let group = await Group.findById(req.body.groupId).populate('members');
-    if (group.length==0) throw new Error('Group with that ID doesnt exist');
+    if (group.length == 0) throw new Error('Group with that ID doesnt exist');
     let readStatus = [];
-    for (let i=0;i<group.members.length;i++){
-        if (group.members[i]._id.toString()!=req.user._id.toString()){
+    for (let i = 0; i < group.members.length; i++) {
+        if (group.members[i]._id.toString() != req.user._id.toString()) {
             readStatus.push(false);
         }
         else readStatus.push(true);
